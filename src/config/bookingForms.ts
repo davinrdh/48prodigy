c6e652df9fc28061d3fadbaaf4d6a8724c7e5099
davinrdh <davinridhas@gmail.com>
2026-09-03 18:27:45 +0700

    update fitur form VC 2S MNG

--- src/config/bookingForms.ts
+++ src/config/bookingForms.ts
@@ -2,9 +2,9 @@ export const bookingFormUrls: Record<string, string> = {
   "video-call":
     "https://docs.google.com/forms/d/e/1FAIpQLSfaHQrC46w6BnR_walI2z4AcS3Js5iRaLNeBTiqwaL7GhG9cw/viewform",
   "2-shot":
-    "https://docs.google.com/forms/d/e/1FAIpQLSfaHQrC46w6BnR_walI2z4AcS3Js5iRaLNeBTiqwaL7GhG9cw/viewform",
+    "https://docs.google.com/forms/d/e/1FAIpQLSdquFX_WrcxOvI8sDlex-9wBEc0fs57qfjliO1odvvU2oklnA/viewform",
   "meet-and-greet":
-    "https://docs.google.com/forms/d/e/1FAIpQLSfaHQrC46w6BnR_walI2z4AcS3Js5iRaLNeBTiqwaL7GhG9cw/viewform",
+    "https://docs.google.com/forms/d/e/1FAIpQLScg0q41em_VMAb3bHGt5h4OzX_U04geRfe0_4TLTWQEFGLBUQ/viewform",
   "concert-jkt48": "https://docs.google.com/forms/d/e/FORM_ID_CONCERT/viewform",
   "general-concert": "https://docs.google.com/forms/d/e/FORM_ID_JOKI/viewform",
 };
